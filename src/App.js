e0cf5915617b2a9594b17286d87ae79d4d93f06d
arpita.sristy <arpita.sristy18@gmail.com>
2021-11-13 14:20:20 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,93 @@
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./App.css";
+import Footer from "./components/Footer/Footer.js";
+import Header from "./components/Header/Header.js";
+import AuthProvider from "./contexts/AuthProvider.js";
+import About from "./pages/About.js";
+import Contact from "./pages/Contact.js";
+import Courses from "./pages/Courses.js";
+import Dashboard from "./pages/Dashboard.js";
+import Details from "./pages/Details.js";
+import Home from "./pages/Home.js";
+import Login from "./pages/Login.js";
+import PageNotFound from "./pages/PageNotFound.js";
+import Reset from "./pages/Reset.js";
+import Signup from "./pages/Signup.js";
+import PrivateRoute from "./route/PrivateRoute.js";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <AuthProvider>
+        <Router>
+          <Switch>
+            <Route exact path="/">
+              <Header> </Header>
+              <Home></Home>
+              <Footer></Footer>
+            </Route>
+
+            <Route path="/home">
+              <Header> </Header>
+              <Home></Home>
+              <Footer></Footer>
+            </Route>
+
+            <Route path="/about">
+              <Header> </Header>
+              <About></About>
+              <Footer></Footer>
+            </Route>
+
+            <Route path="/contact">
+              <Header> </Header>
+              <Contact></Contact>
+              <Footer></Footer>
+            </Route>
+
+            <Route exact path="/services">
+              <Header> </Header>
+              <Courses></Courses>
+              <Footer></Footer>
+            </Route>
+
+            <PrivateRoute path="/dashboard">
+              <Header> </Header>
+              <Dashboard></Dashboard>
+              <Footer></Footer>
+            </PrivateRoute>
+
+            <Route path="/login">
+              <Header> </Header>
+              <Login></Login>
+              <Footer></Footer>
+            </Route>
+
+            <Route path="/signup">
+              <Header> </Header>
+              <Signup></Signup>
+              <Footer></Footer>
+            </Route>
+
+            <Route path="/reset">
+              <Header> </Header>
+              <Reset></Reset>
+              <Footer></Footer>
+            </Route>
+
+            <Route path="/services/:id">
+              <Header> </Header>
+              <Details></Details>
+              <Footer></Footer>
+            </Route>
+
+            <Route path="*">
+              <PageNotFound></PageNotFound>
+            </Route>
+          </Switch>
+        </Router>
+      </AuthProvider>
     </div>
   );
 }
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCDYVc7J2PEiA3mOqMPbLy28fpYcwQ2q1E",
+    authDomain: "khaddo-93002.firebaseapp.com",
+    projectId: "khaddo-93002",
+    storageBucket: "khaddo-93002.appspot.com",
+    messagingSenderId: "184201024781",
+    appId: "1:184201024781:web:05b137e38a50b3e8ee8c44"
+  };
+
+  export default firebaseConfig;
